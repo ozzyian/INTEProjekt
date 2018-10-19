2bda0803949ffdd7e8702ff703183ef8dfca2cf6
Ozzyian <ogustafsson1@gmail.com>
2018-10-19 15:14:14 +0200

    add förberedande för combat utveckling

--- src/main/java/spelKlasser/Combat.java
+++ src/main/java/spelKlasser/Combat.java
@@ -1,5 +1,13 @@
 package spelKlasser;
 
 public class Combat {
-
+	private PlayerCharacter player;
+	private MonsterCharacter monster;
+	
+	public Combat(PlayerCharacter player, MonsterCharacter monster) {
+		this.player = player;
+		this.monster = monster;
+	}
+	
+	
 }
---
+++ src/test/java/testSpelKlasser/TestCombat.java
@@ -0,0 +1,14 @@
+package testSpelKlasser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCombat {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
