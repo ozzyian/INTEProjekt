a2a8afb9e57278b10e31f28cb36823513877b796
vonskareng <43373624+vonskareng@users.noreply.github.com>
2018-10-21 18:22:52 +0200

    TDD 3 attributes (1)

---
+++ src/test/java/testSpelKlasser/TestAttributes.java
@@ -0,0 +1,16 @@
+package testSpelKlasser;
+
+import static org.junit.jupiter.api.Assertions.*;
+import spelKlasser.Attributes; 
+import org.junit.jupiter.api.Test;
+
+class TestAttributes {
+
+	Attribute a = new Attribute();
+	
+	@Test
+	void test() {
+		
+	}
+
+}
