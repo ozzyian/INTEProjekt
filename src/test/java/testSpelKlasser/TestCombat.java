cd674847c9d2994fd96ec2e47e9bf69b5d91c58a
Ozzyian <ogustafsson1@gmail.com>
2018-10-25 19:01:18 +0200

    lol
    d

--- src/test/java/testSpelKlasser/TestCombat.java
+++ src/test/java/testSpelKlasser/TestCombat.java
@@ -3,6 +3,7 @@ package testSpelKlasser;
 import spelKlasser.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -11,37 +12,44 @@ class TestCombat {
 	PlayerCharacter player;
 	Combat c;
  
-	
+	@BeforeEach
+	void setUp() {
+		player = new PlayerCharacter("player");
+		monster = new MonsterCharacter(Difficulty.EASY, "monster");
+		c = new Combat(player,monster);
+	}
 	@Test
 	void testPlayerNull() {
-		c = new Combat(new PlayerCharacter("testP"), new MonsterCharacter(Difficulty.EASY,"testM"));
-		assertNotNull(c.getPlayer());
+		fail("Not yet implemented");
+		
+		//		c = new Combat(new PlayerCharacter("testP"), new MonsterCharacter(Difficulty.EASY,"testM"));
+//		assertNotNull(c.getPlayer());
 	}
-	@Test
-	void testMonsterNull() {
-		c = new Combat(new PlayerCharacter("testP"), new MonsterCharacter(Difficulty.EASY,"testM"));
-		assertNotNull(c.getMonster());
-	}
-	@Test
-	void testSetAttackOrder() {
-		player = new PlayerCharacter("player");
-		monster = new MonsterCharacter(Difficulty.EASY, "monster");
-		player.increaseAgility();
-		c = new Combat(player,monster);
-		c.setAttackOrder();
-		GameCharacter[] expected = {player, monster};
-		assertArrayEquals(expected, c.getAttackOrder());
-	}
-	@Test
-	void testSetFirstAttacker() {
-		player = new PlayerCharacter("player");
-		monster = new MonsterCharacter(Difficulty.EASY, "monster");
-		c = new Combat(player, monster);
-		c.setFirstAttacker(monster);
-		GameCharacter[] expected = {monster, player};
-		assertArrayEquals(expected, c.getAttackOrder());
-	}
-	
+//	@Test
+//	void testMonsterNull() {
+//		c = new Combat(new PlayerCharacter("testP"), new MonsterCharacter(Difficulty.EASY,"testM"));
+//		assertNotNull(c.getMonster());
+//	}
+//	@Test
+//	void testSetAttackOrder() {
+//		//player = new PlayerCharacter("player");
+//		//monster = new MonsterCharacter(Difficulty.EASY, "monster");
+//		player.increaseAgility();
+//		//c = new Combat(player,monster);
+//		c.setAttackOrder();
+//		GameCharacter[] expected = {player, monster};
+//		assertArrayEquals(expected, c.getAttackOrder());
+//	}
+//	@Test
+//	void testSetFirstAttacker() {
+//		//player = new PlayerCharacter("player");
+//		//monster = new MonsterCharacter(Difficulty.EASY, "monster");
+//		//c = new Combat(player, monster);
+//		c.setFirstAttacker(monster);
+//		GameCharacter[] expected = {monster, player};
+//		assertArrayEquals(expected, c.getAttackOrder());
+//	}
+//	
 //	@Test 
 //	void testCheckFearlessActivation() {
 //		player = new PlayerCharacter("player"); 
@@ -66,32 +74,32 @@ class TestCombat {
 //		
 //	}
 	
-	@Test
-	void testCase2() {
-		player = new PlayerCharacter("player");
-		monster = new MonsterCharacter(Difficulty.EASY,"monster");
-		c = new Combat(player,monster);
-		c.setAttackOrder();
-		c.setFirstAttacker(player);
-		player.setAttackDamage(1);
-		monster.setBaseHealth(1);
-		c.startCombat();
-		assertSame(player, c.getWinner());
-	}
+//	@Test
+//	void testCase2() {
+//		//player = new PlayerCharacter("player");
+//		//monster = new MonsterCharacter(Difficulty.EASY,"monster");
+//		//c = new Combat(player,monster);
+//		c.setAttackOrder();
+//		c.setFirstAttacker(player);
+//		player.setAttackDamage(1);
+//		monster.setBaseHealth(1);
+//		c.startCombat();
+//		assertSame(player, c.getWinner());
+//	}
 	
-	@Test
-	void testCase3() {
-		player = new PlayerCharacter("player");
-		monster = new MonsterCharacter(Difficulty.EASY, "monster");
-		c = new Combat(player, monster);
-		player.setBaseHealth(2);
-		monster.setAttackDamage(1);
-		c.setAttackOrder();
-		c.setFirstAttacker(monster);
-		c.startCombat();
-		assertSame(monster, c.getWinner());
-	}
-	
+//	@Test
+//	void testCase3() {
+//		//player = new PlayerCharacter("player");
+//		//monster = new MonsterCharacter(Difficulty.EASY, "monster");
+//		//c = new Combat(player, monster);
+//		player.setBaseHealth(2);
+//		monster.setAttackDamage(1);
+//		c.setAttackOrder();
+//		c.setFirstAttacker(monster);
+//		c.startCombat();
+//		assertSame(monster, c.getWinner());
+//	}
+//	
 	
 	
 
