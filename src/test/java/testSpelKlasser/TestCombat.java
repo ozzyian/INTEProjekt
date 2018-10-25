7c138c2e66d1719e24d9f1c4fadac83795b5aa64
Ozzyian <ogustafsson1@gmail.com>
2018-10-25 19:30:54 +0200

    Cleaned up TestCombat

--- src/test/java/testSpelKlasser/TestCombat.java
+++ src/test/java/testSpelKlasser/TestCombat.java
@@ -3,6 +3,7 @@ package testSpelKlasser;
 import spelKlasser.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -10,33 +11,31 @@ class TestCombat {
 	MonsterCharacter monster;
 	PlayerCharacter player;
 	Combat c;
-
-
+	
+	
+	@BeforeEach
+	void setUp() {
+		player = new PlayerCharacter("player");
+		monster = new MonsterCharacter(Difficulty.EASY, "monster");
+		c = new Combat(player,monster);
+	}
 	@Test
 	void testPlayerNull() {
-		c = new Combat(new PlayerCharacter("testP"), new MonsterCharacter(Difficulty.EASY,"testM"));
 		assertNotNull(c.getPlayer());
 	}
 	@Test
 	void testMonsterNull() {
-		c = new Combat(new PlayerCharacter("testP"), new MonsterCharacter(Difficulty.EASY,"testM"));
 		assertNotNull(c.getMonster());
 	}
 	@Test
 	void testSetAttackOrder() {
-		player = new PlayerCharacter("player");
-		monster = new MonsterCharacter(Difficulty.EASY, "monster");
 		player.increaseAgility();
-		c = new Combat(player,monster);
 		c.setAttackOrder();
 		GameCharacter[] expected = {player, monster};
 		assertArrayEquals(expected, c.getAttackOrder());
 	}
 	@Test
 	void testSetFirstAttacker() {
-		player = new PlayerCharacter("player");
-		monster = new MonsterCharacter(Difficulty.EASY, "monster");
-		c = new Combat(player, monster);
 		c.setFirstAttacker(monster);
 		GameCharacter[] expected = {monster, player};
 		assertArrayEquals(expected, c.getAttackOrder());
@@ -44,18 +43,12 @@ class TestCombat {
 	
 	@Test 
 	void testCheckFearlessActivation() {
-		player = new PlayerCharacter("player"); 
-		monster = new MonsterCharacter(Difficulty.EASY, "monster"); 
-		c = new Combat(player, monster);
 		player.setFearlessActivated(true);
 		assertTrue(c.checkFearlessActivation());
 	}
 	
 	@Test 
 	void testCase1() {
-		player = new PlayerCharacter("player"); 
-		monster = new MonsterCharacter(Difficulty.EASY, "monster"); 
-		c = new Combat(player, monster); 
 		player.increaseAgility(); 
 		player.setBaseHealth(10);
 		monster.setBaseHealth(10);
@@ -68,9 +61,6 @@ class TestCombat {
 	
 	@Test
 	void testCase2() {
-		player = new PlayerCharacter("player");
-		monster = new MonsterCharacter(Difficulty.EASY,"monster");
-		c = new Combat(player,monster);
 		c.setAttackOrder();
 		c.setFirstAttacker(player);
 		player.setAttackDamage(1);
@@ -81,9 +71,6 @@ class TestCombat {
 	
 	@Test
 	void testCase3() {
-		player = new PlayerCharacter("player");
-		monster = new MonsterCharacter(Difficulty.EASY, "monster");
-		c = new Combat(player, monster);
 		player.setBaseHealth(2);
 		monster.setAttackDamage(1);
 		c.setAttackOrder();
