3d6c49e1d8cd8cd61356bc8de2eb12fbe21d6e6a
Ozzyian <ogustafsson1@gmail.com>
2018-10-25 19:23:17 +0200

    Revert "lol"
    This reverts commit cd674847c9d2994fd96ec2e47e9bf69b5d91c58a.

--- src/test/java/testSpelKlasser/TestCombat.java
+++ src/test/java/testSpelKlasser/TestCombat.java
@@ -3,7 +3,6 @@ package testSpelKlasser;
 import spelKlasser.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -13,6 +12,7 @@ class TestCombat {
 	Combat c;
  
 	
+<<<<<<< HEAD
 
 	
 	@Test 
@@ -46,6 +46,88 @@ class TestCombat {
 	}
 	
 
+=======
+	@Test
+	void testPlayerNull() {
+		c = new Combat(new PlayerCharacter("testP"), new MonsterCharacter(Difficulty.EASY,"testM"));
+		assertNotNull(c.getPlayer());
+	}
+	@Test
+	void testMonsterNull() {
+		c = new Combat(new PlayerCharacter("testP"), new MonsterCharacter(Difficulty.EASY,"testM"));
+		assertNotNull(c.getMonster());
+	}
+	@Test
+	void testSetAttackOrder() {
+		player = new PlayerCharacter("player");
+		monster = new MonsterCharacter(Difficulty.EASY, "monster");
+		player.increaseAgility();
+		c = new Combat(player,monster);
+		c.setAttackOrder();
+		GameCharacter[] expected = {player, monster};
+		assertArrayEquals(expected, c.getAttackOrder());
+	}
+	@Test
+	void testSetFirstAttacker() {
+		player = new PlayerCharacter("player");
+		monster = new MonsterCharacter(Difficulty.EASY, "monster");
+		c = new Combat(player, monster);
+		c.setFirstAttacker(monster);
+		GameCharacter[] expected = {monster, player};
+		assertArrayEquals(expected, c.getAttackOrder());
+	}
+	
+//	@Test 
+//	void testCheckFearlessActivation() {
+//		player = new PlayerCharacter("player"); 
+//		monster = new MonsterCharacter(Difficulty.EASY, "monster"); 
+//		c = new Combat(player, monster);
+//		player.setFearlessActivated(true);
+//		assertTrue(c.checkFearlessActivation());
+//	}
+	
+//	@Test 
+//	void testCase1() {
+//		player = new PlayerCharacter("player"); 
+//		monster = new MonsterCharacter(Difficulty.EASY, "monster"); 
+//		c = new Combat(player, monster); 
+//		player.increaseAgility(); 
+//		player.setBaseHealth(10);
+//		monster.setBaseHealth(10);
+//		monster.setAttackDamage(3);
+//		c.setAttackOrder(); 
+//		c.startCombat();
+//		assertEquals(player, c.getWinner());
+//		
+//	}
+	
+	@Test
+	void testCase2() {
+		player = new PlayerCharacter("player");
+		monster = new MonsterCharacter(Difficulty.EASY,"monster");
+		c = new Combat(player,monster);
+		c.setAttackOrder();
+		c.setFirstAttacker(player);
+		player.setAttackDamage(1);
+		monster.setBaseHealth(1);
+		c.startCombat();
+		assertSame(player, c.getWinner());
+	}
+	
+	@Test
+	void testCase3() {
+		player = new PlayerCharacter("player");
+		monster = new MonsterCharacter(Difficulty.EASY, "monster");
+		c = new Combat(player, monster);
+		player.setBaseHealth(2);
+		monster.setAttackDamage(1);
+		c.setAttackOrder();
+		c.setFirstAttacker(monster);
+		c.startCombat();
+		assertSame(monster, c.getWinner());
+	}
+	
+>>>>>>> parent of cd67484... lol
 	
 	
 
